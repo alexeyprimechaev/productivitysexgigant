041bc6b7112623addfcd0526eddd523d17bc3caa
igor <write.igor@gmail.com>
2021-12-04 15:15:57 +0300

    Initial Commit

---
+++ productivitysexgigant/productivitysexgigantApp.swift
@@ -0,0 +1,17 @@
+//
+//  productivitysexgigantApp.swift
+//  productivitysexgigant
+//
+//  Created by Игорь Дячук on 04.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct productivitysexgigantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
