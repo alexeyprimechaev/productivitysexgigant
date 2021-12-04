9bc158ba0029ffdb406904e1cf3c7980e4ca2e4e
alexeyprimechaev <51163708+alexeyprimechaev@users.noreply.github.com>
2021-12-04 16:26:15 +0300

    komit

---
+++ productivitysexgigant/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  productivitysexgigant
+//
+//  Created by Alexey Primechaev on 12/4/21.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
